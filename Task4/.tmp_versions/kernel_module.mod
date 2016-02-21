c6ac1d8662a9fa19017d2b3ee269c731c77e7263
root <root@debian.debian>
2016-02-21 12:14:04 -0500

    Task 4 basic functionality done

---
+++ Task4/.tmp_versions/kernel_module.mod
@@ -0,0 +1,2 @@
+/root/exam/Task4/kernel_module.ko
+/root/exam/Task4/kernel_module.o
---
+++ Task4/kernel_module.c
@@ -0,0 +1,50 @@
+#include <linux/module.h>
+#include <linux/proc_fs.h>
+#include <linux/seq_file.h>
+
+#define PROC_NAME "justASimpleModule"
+#define LOG_LEVEL KERN_ALERT
+
+MODULE_AUTHOR("Slavi Georgiev");
+MODULE_DESCRIPTION("Takes a string argument and stores it in /proc.");
+MODULE_LICENSE("GPL");
+
+static char* strArg; // `static' not needed but better
+module_param(strArg, charp, 0); // name, type, permissions
+
+static int show(struct seq_file *m, void *v)
+{
+	seq_printf(m, "String argument `strArg': %s\n", strArg);
+	return 0; // success
+}
+
+static int open(struct inode *inode, struct file *file)
+{
+	return single_open(file, show, NULL);
+}
+
+static const struct file_operations fops =
+{
+	.owner = THIS_MODULE,
+	.open = open,
+	.read = seq_read,
+	.llseek = seq_lseek,
+	.release = single_release
+};
+
+static int __init init(void)
+{
+	proc_create(PROC_NAME, 0, NULL, &fops);
+	return 0;
+}
+
+static void __exit exit(void)
+{
+	printk(LOG_LEVEL "Module unloaded.\n");
+	// This message would be seen with `dmesg'.
+	remove_proc_entry(PROC_NAME, NULL); //
+	// uncomment the previous line if needed
+}
+
+module_init(init);
+module_exit(exit);
